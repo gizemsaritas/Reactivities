c264eadb048e80e2da852ccaa4de7af6acb0a618
gizemsaritas <ymgcon@hotmail.com>
2021-01-19 15:55:11 +0300

    added Navbar

--- Reactivities/reactivities-react/src/app/layout/App.tsx
+++ Reactivities/reactivities-react/src/app/layout/App.tsx
@@ -2,6 +2,7 @@ import React, { useState,useEffect} from 'react';
 import axios from 'axios';
 import { Header, Icon, List } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
+import { NavBar } from '../../features/nav/NavBar';
 
 function App() {
 	const [ activities, setActivities ] = useState<IActivity[]>([]);
@@ -13,10 +14,7 @@ function App() {
 	},[]);
 	return (
 		<div>
-			<Header as="h2">
-				<Icon name="users" />
-				<Header.Content>Reactivities</Header.Content>
-			</Header>
+			<NavBar/>
 			<List>{activities.map((activity) => <List.Item key={activity.id}>{activity.title}</List.Item>)}</List>
 		</div>
 	);
