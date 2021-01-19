bdb7ba978b3e831a897c9941a469898432251fe3
gizemsaritas <ymgcon@hotmail.com>
2021-01-19 15:40:18 +0300

    react get data list  with hooks and axios

---
+++ Reactivities/reactivities-react/src/app/layout/App.tsx
@@ -0,0 +1,25 @@
+import React, { useState,useEffect} from 'react';
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
+import { IActivity } from '../models/activity';
+
+function App() {
+	const [ activities, setActivities ] = useState<IActivity[]>([]);
+	useEffect(() => {
+		axios.get<IActivity[]>("https://localhost:44321/api/activities")
+		.then((response)=>{
+			setActivities(response.data)
+		});
+	},[]);
+	return (
+		<div>
+			<Header as="h2">
+				<Icon name="users" />
+				<Header.Content>Reactivities</Header.Content>
+			</Header>
+			<List>{activities.map((activity) => <List.Item key={activity.id}>{activity.title}</List.Item>)}</List>
+		</div>
+	);
+}
+
+export default App;
